c029f77740bee1f6e50da66774a1b687d8ce4312
Blackburn0 <hamzatadebayo5@gmail.com>
2023-01-08 07:38:35 +0100

    Frist commit

---
+++ HackRank 30DaysOfCode Challenge (Python 3)/Day 1/main.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+int_two = int(input())
+double_two = float(input())
+string_two = input()
+
+print(i + int_two)
+print(d + double_two)
+print(s + string_two)
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
